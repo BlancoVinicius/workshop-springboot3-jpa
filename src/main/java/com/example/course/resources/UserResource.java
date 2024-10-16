15f70c86a21b3f1372019f51aae9e4032081b61e
vinicius <blanco.vinicius@uni9.edu.br>
2024-10-16 17:19:01 -0300

    User update

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,33 +21,39 @@ import com.example.course.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 
 		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User u = userService.findById(id);
 		return ResponseEntity.ok().body(u);
 	}
 
 	@PostMapping
-	public ResponseEntity<User> isert(@RequestBody User obj){
+	public ResponseEntity<User> isert(@RequestBody User obj) {
 		obj = userService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);	
+		return ResponseEntity.created(uri).body(obj);
 	}
-	
+
 	@DeleteMapping(value = "{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "{id}")
+	public ResponseEntity<User> updatee(@PathVariable Long id, @RequestBody User obj){
+		userService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
